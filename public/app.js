b96516b0c69a54c4c44b8239ee7233cec2cec48b
Graham Desmond <gdesmond@pdx.edu>
2021-03-31 11:45:12 -0700

    git

---
+++ public/app.js
@@ -0,0 +1,24 @@
+//App.js includes animations and logic defining the header menu.
+
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav1 = document.querySelector('.background');
+    const navLinkRight = document.querySelector('.navLinkRight li');
+    const navLinkLeft = document.querySelector('.navLinkLeft li');
+
+    burger.addEventListener('click', () => {
+        nav1.classList.toggle('nav1-active');
+    //Animate Links
+    if(navLinkLeft.style.animation) {
+        navLinkLeft.style.animation = ``;
+        navLinkRight.style.animation = ``;
+    } else {
+        navLinkRight.style.animation = `navLinkFade 0.5s ease forwards ${0/7 + 0.5}s`
+        navLinkLeft.style.animation = `navLinkFade 0.5s ease forwards ${1/7 + 0.5}s`
+    }
+
+    burger.classList.toggle('toggle');
+
+    });
+  }
+  navSlide();
