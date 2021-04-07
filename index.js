c2ae2d8565f035c9a7cc7ff4dc411110a36507cd
Graham Desmond <gdesmond@pdx.edu>
2021-04-07 13:12:59 -0700

    I added chat.html to the functionality of the website. I also added some comments to chat.html to clarify where things are happening. I figured out the problem with sendIcon.png so the image should be loading properly now.

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const PORT = process.env.PORT || 3000;
 
 const INDEX = "/index.html";
 const STUDENT = "public/student.html";
+const CHAT = "public/chat.html";
 const ADMIN = "public/admin.html";
 const ABOUT = "public/about.html";
 const MESSAGE = "public/message.html";
@@ -29,6 +30,11 @@ app.get("/", (req, res) => {
 
 app.get("/student", (req, res) => {
   res.sendFile(STUDENT, { root: __dirname });
+});
+
+
+app.get("/chat", (req, res) => {
+  res.sendFile(CHAT, { root: __dirname });
 });
 
 app.get("/admin", (req, res) => {
