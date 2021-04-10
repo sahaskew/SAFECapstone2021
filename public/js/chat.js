4e274ed7c95281333323b7d3659988b27052a5f2
kelsey-debug <kelsey36@pdx.edu>
2021-04-10 15:15:59 -0700

    basic chat working, need some frontend formatting

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,10 +1,12 @@
 /* this file is ran when chat.html is loaded. 
-It is socket connection create via frontend */ 
+It is  the socket connection create via frontend .
+ note that for ANY message sent it is relayed 
+ through the socket and server before being 
+ displayed on front end. So app.js cant implement msg retrieval. */ 
 
-//var socket = io.connect( 'http://localhost:3000' );
+ //'http://localhost:3000' 
 
 const socket = io(); //create frontend socket connection to server. 
-
 window.onload=function(){
 
  const chatForm  = document.getElementById("form");
@@ -12,13 +14,15 @@ window.onload=function(){
   chatForm.addEventListener('submit', event => {
    event.preventDefault(); //so that form contents do not automatically send to a file/ disconnect socket
    const msg = document.getElementById("msg").value; 
-  // const msg = event.target.elements.msg.value; //get the msg value from the form
-
-   //test msg output
-//   console.log(msg);
-
-   //send message to server
+  // const msg = event.target.elements.msg.value;
+   
+  //send message to server
    socket.emit('chatMsg', msg); 
+  
+   //clear input after sending msg
+   document.getElementById("msg").value = '';
+
+
   });
  }
 
@@ -29,18 +33,15 @@ socket.on('Msg', msg => {
   chatBox(msg) //now send that message to be put in DOM as chatbox
 });
 
-//output message to DOM to create chatboxes
+//output message to DOM to inside chatboxes
 function chatBox(msg){
  const div = document.createElement('div');
  div.classList.add('.mess');
  div.innerHTML = `<p class= "messageText"> ${msg} </p>`;
- document.querySelector('.mess').appendChild(div);
+ document.querySelector('.chatBox .mess').appendChild(div);
 }
-//chatR chatL or chatbox class
 
-//when msg submiitted, (1)display on client side (2) emit it to the server 
-//call below function when button clicked
-/*
+/* not in use
  document.getElementById("submit-button").addEventListener('click', function() {
   const msg = document.getElementById("msg").value; 
   console.log(msg);
