896a80094446c0dd6394d713034f159eefc0508f
Dom26 <domniniquemoore12@yahoo.com>
2021-05-21 22:35:20 -0700

    fixed issue where login submit goes to dashboard when email and password are empty

--- public/js/login.js
+++ public/js/login.js
@@ -16,43 +16,6 @@
 // setup: Have this file track the admin.html file
 // have the script tag in the header of the admin.html file list this file
 // as the source
-// Next: create a socket.io instance
-//const socket = io();
-
-// Function runs as soon as the admin.html file is loaded
-/*
-window.onload = function() {
-    const loginForm = document.getElementById("login-form");
-    if(loginForm) {
-        loginForm.addEventListener('submit', event => {
-            // I'm not sure if I need to do this
-            event.preventDefault();
-            // Grab the email and password
-            // Note: Both the email and password are required in the html
-            //       form, so it's important that the following lines don't
-            //       execute if the user leaves any of the fields blank.
-            //       Test for later
-            // It does seem like the html form doesn't send the information
-            // through when a field is left blank
-            const email = document.getElementById("email").value;
-            const password = document.getElementById("password").value;
-
-            // DON'T KEEP THIS!!! FOR TESTING PURPOSES ONLY!!!
-            alert("Email: " + email + "\nPassword: " + password);
-            // Was successful in getting the user's data
-            // Remember to delete this once finished integrating the database
-
-            // DO NOT KEEP THIS!! FOR TESTING PURPOSES ONLY!!!!
-            // It didn't work. Still need to delete this just in case
-            //socket.emit('message', email, password);
-
-            // Clear inputs after login attempt
-            document.getElementById("email").value = '';
-            document.getElementById("password").value = '';
-        });
-    }
-}
-*/
 const fs = require('fs');
 
 const user = {email:'safeadmin@safemail.com', password:'safepassword'};
